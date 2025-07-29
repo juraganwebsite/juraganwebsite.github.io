0fcb4303a37b456f8d78972ad1f16c3cd4ecca50
juraganwebsite <tim.juraganweb@gmail.com>
2025-07-29 17:50:16 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Simpan semua konfigurasi global di sini.
+
+const DEFAULT_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzOwZiLaMc1vIILdAoSPDXugJE_TIWiBYYdlawkz48XMgb6qgEnOVxpcz4KU-kPeNQy/exec';
